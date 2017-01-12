820dc192eebfc3194040a099f5d3006504c8fe79
natnayr <ryantanjunming@gmail.com>
2017-01-12 15:53:08 +0800

    test

--- app/src/main/java/com/crowdo/p2pmobile/activity/WelcomeActivity.java
+++ app/src/main/java/com/crowdo/p2pmobile/activity/WelcomeActivity.java
@@ -72,6 +72,7 @@ public class WelcomeActivity extends AppCompatActivity implements MediaPlayer.On
                 }catch (IOException e){
                     Log.e(LOG_TAG, "ERROR: surfaceCreated error " + e.getMessage(), e);
                     e.printStackTrace();
+                    
                 }
             }
 
