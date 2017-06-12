8da7d1d105b9f226e9dff63288cddd6b01d24432
natnayr <ryantanjunming@gmail.com>
2017-06-12 20:06:12 +0800

    member info retrieval halfway

---
+++ app/src/main/java/com/crowdo/p2pconnect/commons/MemberDataRetrieval.java
@@ -0,0 +1,22 @@
+package com.crowdo.p2pconnect.commons;
+
+import android.app.Activity;
+
+import com.crowdo.p2pconnect.model.response.MemberInfoResponse;
+
+/**
+ * Created by cwdsg05 on 12/6/17.
+ */
+
+public class MemberDataRetrieval {
+
+    public static final String LOG_TAG = MemberDataRetrieval.class.getSimpleName();
+
+    public static MemberInfoResponse retrieveMemberInfo(final Activity activity){
+
+        
+
+    }
+
+
+}
